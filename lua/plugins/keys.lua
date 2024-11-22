0d86716e65cb67e2620283482627b05c2684db40
Iordanis Petkakis <12776461+dpetka2001@users.noreply.github.com>
2024-11-22 22:59:00 +0200

    feat: add `showkeys`

---
+++ lua/plugins/keys.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "nvchad/showkeys",
+    -- dev = true,
+    cmd = "ShowkeysToggle",
+    opts = {
+      timeout = 1,
+      maxkeys = 5,
+      position = "top-right",
+      winopts = {
+        zindex = 100,
+      },
+      -- show_count = true,
+      -- more opts
+    },
+  },
+}
